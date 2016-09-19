3930a8a0134e3ed63e665aeaabf18865c7c8f381
Ryan <rblakeman@csumb.edu>
2016-09-19 10:58:43 -0700

    Creates blank white picture

--- project2.py
+++ project2.py
@@ -2,11 +2,13 @@
 # Project2.py
 # github.com/rblakeman/CST205-Project2
 
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageFont, ImageDraw
+
+originalImage = Image
 
 try:
     originalImage = Image.open("pic.png")
-except
+except:
      print ("Failed to load")
 
 newImage = Image.new("RGB", originalImage.size, (255,255,255))
