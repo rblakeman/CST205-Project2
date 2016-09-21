e9225a91f0f4487a37d6e7e8cd03c2669093be9a
Ryan <rblakeman@csumb.edu>
2016-09-21 11:50:54 -0700

    Unfinished progress

--- project2.py
+++ project2.py
@@ -1,7 +1,7 @@
 # CST 205
 # Project2.py
 # github.com/rblakeman/CST205-Project2
-import PIL
+import PIL, sys
 from PIL import Image, ImageFilter, ImageFont, ImageDraw
 
 #definitions
@@ -9,10 +9,14 @@ originalImage = Image
 red = 0
 green = 0
 blue = 0
+firstpart = "string1"
+secondpart = "string2"
+thirdpart = "string3"
+fourthpart = "string4"
 
 #open original image
 try:
-    originalImage = Image.open("pic2.jpg")
+    originalImage = Image.open("pic.png")
 except:
      print ("Failed to load")
 
@@ -21,20 +25,56 @@ newImage = Image.new("RGB", originalImage.size, (0,0,0))
 newImagedata = newImage.load()
 pictureWidth = originalImage.size[0]
 pictureHeight = originalImage.size[1]
+rgb_og = originalImage.convert('RGB') # conversion error check
 for x in range(0,pictureWidth):
     for y in range(0, pictureHeight):
-        rgb_og = originalImage.convert('RGB')
         red, green, blue = rgb_og.getpixel((x,y))
         newImagedata[x,y] = (red, green, blue)
 
 #meme it
 font = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSans.ttf", 25)
 draw = ImageDraw.Draw(newImage)
+text = "A really really really really long test meme"
+lineCount = 1
+textWidth, textHeight = font.getsize(text)# gets the width and height of the text macro font
+if textWidth > pictureWidth:
+    if textWidth / 2*pictureWidth > 1:       # 2 lines of text
+        firstpart, secondpart = text[:int(len(text)/2)], text[int(len(text)/2):]
+        lineCount = 2
+    elif textWidth / 2 > 1:     # 3 lines of text
+        #firstpart, secondpart, thirdpart = string[:len(text)/3], string[len(text)/3], string[len(text)/3:]
+        lineCount = 3
+    elif textWidth / 3 > 1:     # 4 lines of text
+        lineCount = 4
+    else:
+        print ("Error: Text is too long")
+        sys.exit()
 
-text = "Test Meme"
-length = len(text) / 2
-center = pictureWidth / 2
-draw.text((center - length,0), text, (255,255,255), font=font)
+for j in range(1,lineCount):
+    if (lineCount > 1):
+        if j == 1:
+            textWidth, textHeight = font.getsize(firstpart)
+            text = firstpart
+        elif j == 2:
+            textWidth, textHeight = font.getsize(secondpart)
+            text = secondpart
+        elif j == 3:
+            textWidth, textHeight = font.getsize(thirdpart)
+            text = thirdpart
+        elif j == 4:
+            textWidth, textHeight = font.getsize(fourthpart)
+            text = fourthpart
+        else:
+            print ("Error in linecount")
+
+    length = textWidth / len(text)      # takes the width of the line of text and divides by the number of characters
+    length = length * (len(text)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    if j > 1:
+        height = textHeight * j-1
+    else:
+        height = 0
+    draw.text((center - length, height), text, (255,255,255), font=font)
 
 #save it
 newImage.save("meme.png")
