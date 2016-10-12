2dd37edc8e6006d31e98b9a7f2a87470b0bb112a
rblakeman <rblakeman@csumb.edu>
2016-10-12 10:20:54 -0700

    v1.0 of meme caption
    Final code for actual caption generation part

--- project2.py
+++ project2.py
@@ -19,11 +19,13 @@ red = 0
 green = 0
 blue = 0
 text = "text"
+text2 = "text2"
 firstpart = "string1"
 secondpart = "string2"
 thirdpart = "string3"
 fourthpart = "string4"
 temp = "list"
+temp2 = "list2"
 
 location = str(input('Enter picture name: '))
 #open original image
@@ -49,31 +51,26 @@ for x in range(0,pictureWidth):
 #meme it
 font = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSans.ttf", 25)
 draw = ImageDraw.Draw(newImage)
-text = str(input('Enter meme text: '))
+###################################################################################################
+text = str(input('Enter top text: '))
 lineCount = 1
-textWidth, textHeight = font.getsize(text)# gets the width and height of the text macro font
+textWidth, textHeight = font.getsize(text) # gets the width and height of the text macro font
 if textWidth > pictureWidth:
     if textWidth / pictureWidth > 1 and textWidth / pictureWidth < 2:       # 2 lines of text
         lineCount = 2
         print ("2 lines of text")
-        #firstpart, secondpart = text[:int(len(text)/2)], text[int(len(text)/2):]
-        nth = (len(text) / 2) + 1
+        nth = (len(text) / 2) + 2
         temp = textwrap.wrap(text, nth)
         print (temp)
-        #print len(temp[0])
-        #print len(temp[1])
         firstpart = temp[0]
         secondpart = temp[1]
 
     elif textWidth / pictureWidth >= 2 and textWidth / pictureWidth < 3:     # 3 lines of text
         lineCount = 3
         print ("3 lines of text")
-        nth = (len(text) / 3) + 1
+        nth = (len(text) / 3) + 2
         temp = textwrap.wrap(text, nth)
         print (temp)
-        #print len(temp[0])
-        #print len(temp[1])
-        #print len(temp[2])
         firstpart = temp[0]
         secondpart = temp[1]
         thirdpart = temp[2]
@@ -81,13 +78,9 @@ if textWidth > pictureWidth:
     elif textWidth / pictureWidth >= 3 and textWidth / pictureWidth < 4:     # 4 lines of text
         lineCount = 4
         print ("4 lines of text")
-        nth = len(text) / 4 + 1
+        nth = len(text) / 4 + 2
         temp = textwrap.wrap(text, nth)
         print(temp)
-        #print len(temp[0])
-        #print len(temp[1])
-        #print len(temp[2])
-        #print len(temp[3])
         firstpart = temp[0]
         secondpart = temp[1]
         thirdpart = temp[2]
@@ -124,6 +117,162 @@ for j in range(0,lineCount):
     else:
         height = 0
     draw.text((center - length, height), text, (255,255,255), font=font)
+
+###################################################################################################
+text2 = str(input('Enter bottom text: '))
+lineCount = 1
+textWidth, textHeight = font.getsize(text2) # gets the width and height of the text2 macro font
+if textWidth > pictureWidth:
+    if textWidth / pictureWidth > 1 and textWidth / pictureWidth < 2:       # 2 lines of text
+        lineCount = 2
+        print ("2 lines of text2")
+        nth = (len(text2) / 2) + 2
+        temp2 = textwrap.wrap(text2, nth)
+        print (temp2)
+        firstpart = temp2[0]
+        secondpart = temp2[1]
+
+    elif textWidth / pictureWidth >= 2 and textWidth / pictureWidth < 3:     # 3 lines of text
+        lineCount = 3
+        print ("3 lines of text2")
+        nth = (len(text2) / 3) + 2
+        temp2 = textwrap.wrap(text2, nth)
+        print (temp2)
+        firstpart = temp2[0]
+        secondpart = temp2[1]
+        thirdpart = temp2[2]
+
+    elif textWidth / pictureWidth >= 3 and textWidth / pictureWidth < 4:     # 4 lines of text
+        lineCount = 4
+        print ("4 lines of text2")
+        nth = len(text2) / 4 + 2
+        temp2 = textwrap.wrap(text2, nth)
+        print(temp2)
+        firstpart = temp2[0]
+        secondpart = temp2[1]
+        thirdpart = temp2[2]
+        fourthpart = temp2[3]
+
+    elif textWidth / pictureWidth >= 4:    # passed max
+        print ("Error: text2 is too long")
+        sys.exit()
+else:
+    print("1 line of text2")
+
+k = 1
+for h in range(lineCount, 0, -1):
+    if (lineCount > 1):
+        if h == 1:
+            textWidth, textHeight = font.getsize(firstpart)
+            text2 = firstpart
+        elif h == 2:
+            textWidth, textHeight = font.getsize(secondpart)
+            text2 = secondpart
+        elif h == 3:
+            textWidth, textHeight = font.getsize(thirdpart)
+            text2 = thirdpart
+        elif h == 4:
+            textWidth, textHeight = font.getsize(fourthpart)
+            text2 = fourthpart
+        else:
+            print ("Error in linecount")
+
+    length = textWidth / len(text2)      # takes the width of the line of text and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    if h > 0:
+        height = pictureHeight - (textHeight * k)
+    else:
+        height = pictureHeight - textHeight
+    draw.text((center - length, height-2), text2, (255,255,255), font=font)
+    k = k+1
+"""
+if lineCount == 1:
+    textWidth, textHeight = font.getsize(firstpart)
+    text2 == firstpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - textHeight
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+if lineCount == 2:
+    textWidth, textHeight = font.getsize(firstpart)
+    text2 == firstpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - (textHeight*2)
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+    textWidth, textHeight = font.getsize(secondpart)
+    text2 == secondpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - textHeight
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+if lineCount == 3:
+    textWidth, textHeight = font.getsize(firstpart)
+    text2 == firstpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - (textHeight*3)
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+    textWidth, textHeight = font.getsize(secondpart)
+    text2 == secondpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - (textHeight*2)
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+    textWidth, textHeight = font.getsize(thirdpart)
+    text2 == thirdpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - textHeight
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+if lineCount == 4:
+    textWidth, textHeight = font.getsize(firstpart)
+    text2 == firstpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - (textHeight*4)
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+    textWidth, textHeight = font.getsize(secondpart)
+    text2 == secondpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - (textHeight*3)
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+    textWidth, textHeight = font.getsize(thirdpart)
+    text2 == thirdpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - (textHeight*2)
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+    textWidth, textHeight = font.getsize(fourthpart)
+    text2 == fourthpart
+    length = textWidth / len(text2)      # takes the width of the line of text2 and divides by the number of characters
+    length = length * (len(text2)/2)     # takes the individual char length and multiplies it by the amount of chars on the left side
+    center = pictureWidth / 2           # gets the center point of the picture width
+    height = pictureHeight - textHeight
+    draw.text((center - length, height), text2, (255,255,255), font=font)
+
+elif lineCount > 4:
+    print ("Error in linecount") """
 
 #save it
 newImage.save("meme.png")
